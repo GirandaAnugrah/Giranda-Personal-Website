35a7995d924d55b26ab746c6cba1dea5a583d1f5
Giranda Anugrah <96484451+GirandaAnugrah@users.noreply.github.com>
2024-01-13 20:26:22 +0700

    final

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -4,7 +4,7 @@ function Projects() {
   return (
     <div className="flex justify-around flex-wrap min-h-screen bg-primary pt-20">
         <div className="desc lg:w-5/12 self-center ms-6">
-          <h1 className="text-6xl md:text-8xl text-white font-mono">My <span className="text-secondary"><TextWriter text="Project" delay={250} infinite/></span></h1>
+          <h1 className="text-5xl md:text-8xl text-white font-mono">My <span className="text-secondary"><TextWriter text="Project" delay={250} infinite/></span></h1>
           <p className="text-white text-sm md:text-lg font-mono my-3 break-words">Passionate about web development, my current project is an exciting journey. I dive into creative ideas, constantly pushing boundaries to create exceptional online experiences. It's a thrilling process, and I'm motivated to keep crafting impressive websites that leave a lasting impact.</p>
           <a href="https://www.umnfestival.com" target="_blank">
             <button className="btn btn-xs sm:btn-sm md:btn-md bg-secondary font-bold lg:text-lg font-mono">Preview</button>
