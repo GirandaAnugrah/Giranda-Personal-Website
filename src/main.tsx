edb686992610f497509851f1f6b3c4235ae050e5
Giranda Anugrah <96484451+GirandaAnugrah@users.noreply.github.com>
2024-05-09 21:33:26 +0700

    feat: loading

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import Home from './pages/Home.tsx'
+import React, { Suspense } from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import Home from "./pages/Home.tsx";
+import LoadingFallback from "./components/LoadingFallback.tsx";
 
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Home/>
-  </React.StrictMode>,
-)
+    <Suspense fallback={<LoadingFallback />}>
+      <Home />
+    </Suspense>
+  </React.StrictMode>
+);
